7d660f79be8334c49084a57eb4944dd5062f80ca
MackRoe <elaine.music@students.makeschool.com>
2020-11-05 18:23:16 -0600

    code merge function

--- sorting_recursive.py
+++ sorting_recursive.py
@@ -9,6 +9,30 @@ def merge(items1, items2):
     # TODO: Repeat until one list is empty
     # TODO: Find minimum item in both lists and append it to new list
     # TODO: Append remaining items in non-empty list to new list
+    left_pointer = 0
+    right_pointer = 0
+    arr = []
+
+    while left_pointer < len(left_arr) and right_pointer < len(right_arr):
+        if left_arr[left_pointer] < right_arr[right_pointer]:
+            arr.append(left_arr[left_pointer])
+            left_pointer += 1
+        elif right_arr[right_pointer] < left_arr[left_pointer]:
+            arr.append(right_arr[right_pointer])
+            right_pointer += 1
+        elif left_arr[left_pointer] == right_arr[right_pointer]:
+            arr.append(left_arr[left_pointer])
+            arr.append(right_arr[right_pointer])
+            left_pointer += 1
+            right_pointer += 1
+
+    for i in range(left_pointer, len(left_arr)):
+        arr.append(left_arr[i])
+    for i in range(right_pointer, len(right_arr)):
+        arr.append(right_arr[i])
+    return arr
+
+
 
 
 def split_sort_merge(items):
@@ -31,6 +55,24 @@ def merge_sort(items):
     # TODO: Split items list into approximately equal halves
     # TODO: Sort each half by recursively calling merge sort
     # TODO: Merge sorted halves into one list in sorted order
+
+    # initialize variables
+    pointer1 = 0
+    pointer2 = 0
+
+    # base case
+    if len(array) == 1:  # a list of 1 is already sorted
+        return array
+
+    # recursive case
+    middle = len(array) // 2
+    # gimmie the left chunk and the right chunk
+    left = array[0:middle + 1]
+    right = array[middle + 1:]
+
+    resultleft = merge_sort(left)
+    resultright = merge_sort(right)
+    return merge(resultleft, resultright)
 
 
 def partition(items, low, high):
