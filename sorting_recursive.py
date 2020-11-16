ecb5d3b1f23412682c5e6465b7a16f7c2aac5610
MackRoe <elaine.music@students.makeschool.com>
2020-11-16 14:34:24 -0500

    modify comments to include time and space complexity answers

--- sorting_recursive.py
+++ sorting_recursive.py
@@ -4,8 +4,8 @@
 def merge(items1, items2):
     """Merge given lists of items, each assumed to already be in sorted order,
     and return a new list containing all items in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    Running time: O(n log(n)) under all conditions (same as merge_sort)
+    Memory usage: O(n) under worst case condition"""
     # TODO: Repeat until one list is empty
     # TODO: Find minimum item in both lists and append it to new list
     # TODO: Append remaining items in non-empty list to new list
@@ -40,8 +40,8 @@ def split_sort_merge(items):
     """Sort given items by splitting list into two approximately equal halves,
     sorting each with an iterative sorting algorithm, and merging results into
     a list in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    Running time & Memory usage: Depend on the sort used
+    """
     # TODO: Split items list into approximately equal halves
     left, right = split(items)
     # TODO: Sort each half using any other sorting algorithm
@@ -52,8 +52,9 @@ def split_sort_merge(items):
 def merge_sort(items):
     """Sort given items by splitting list into two approximately equal halves,
     sorting each recursively, and merging results into a list in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    Running time: O(n log(n)) under all conditions
+    Memory usage: O(n) under worst case condition
+    """
     # Check if list is so small it's already sorted (base case)
     if is_sorted(items):
         return items
@@ -82,8 +83,8 @@ def partition(items, start, end):
     `[low...high]` by choosing a pivot (midpoint method) from
     that range, moving pivot into index `p`, items less than pivot into range
     `[low...p-1]`, and items greater than pivot into range `[p+1...high]`.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    Running time: O(n*m) Has while loops
+    """
     # TODO: Choose a pivot any way and document your method in docstring above
     # p is partition boundary
     pivot = midpoint(items)
@@ -120,9 +121,11 @@ def partition(items, start, end):
 def quick_sort(items, low=None, high=None):
     """Sort given items in place by partitioning items in range `[low...high]`
     around a pivot item and recursively sorting each remaining sublist range.
-    TODO: Best case running time: ??? Why and under what conditions?
-    TODO: Worst case running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    Best case running time: Θ(n log(n))
+    Worst case running time: O(n^2)
+    Memory usage: O(log(n))
+    https://www.bigocheatsheet.com
+    """
     # TODO: Check if high and low range bounds have default values (not given)
     # TODO: Check if list or range is so small it's already sorted (base case)
 
